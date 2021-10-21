35749e8f306a2ca01ce0268928dcfb0e72c3607c
Dany Boza <db@backium.co>
2021-10-20 23:47:27 -0500

    display dynamic text

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import Editor, { EditorProvider, useHandlers } from '../dist'
+import Editor, { EditorProvider, useHandlers } from '../src'
 
 const template = {
   name: 'Untitled design',
@@ -60,7 +60,7 @@ const App = () => {
   // }, [handlers])
   const handlerDownload = async () => {
     if (handlers) {
-      const template = await handlers.designHandler.toDataURL()
+      const template = await handlers.designHandler.toDataURL({ text: 'Hello moon' })
       console.log(template)
     }
   }
